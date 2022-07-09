f176a42c4913fc5af779f512cb595a3da1093ac2
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-07-09 02:25:36 -0300

    Challenge four completed

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const arr1 = [1, 2, -2, 4];
 function run(arr) {
   if (arr.length > 0) {
     const arrExpected = arr.filter((el) => el < 0);
-      if (arrExpected.length == 0) {
-        return "No negative numbers were found";
-      } else {
-        return arrExpected;
-      }
+    if (arrExpected.length == 0) {
+      return "No negative numbers were found";
+    } else {
+      return arrExpected;
     }
   }
+}
 
 console.log(run(arr1));
 
@@ -21,7 +21,7 @@ console.log(run(arr1));
 // Write a function that takes an array (a) and a value (n) as argument
 // Return the nth element of 'a
 
-const arr2 = [7,2,1,6,3];
+const arr2 = [7, 2, 1, 6, 3];
 const num2 = 1
 
 function run(arr, num) {
@@ -37,16 +37,28 @@ console.log(run(arr2, num2));
 // Remove the first 3 elements of 'a'
 // Return the result
 
-const arr = [1,2,4,4,5, 7 ,8 ,7];
+const arr3 = [1, 2, 4, 4, 5, 7, 8, 7];
 
 function run(arr) {
   arr.splice(0, 3)
   return arr
 }
-console.log(run(arr));
+console.log(run(arr3));
 
 
-//--------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
 
 
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
 
+
+const arr4 = [9, 4, 4, 4, 4];
+
+function run(arr) {
+  const initialCount = arr.map((el, index) => index + 1)
+  const finalCount = initialCount.length
+  return finalCount
+}
+
+console.log(run(arr4));
